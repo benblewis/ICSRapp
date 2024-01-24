ceb2e984ea8ca4b6c8e75f54fadefdbd3bfd7d16
benblewis <lewis899@mit.edu>
2024-01-24 13:33:09 -0500

    just pushing the updates that i've made. not quite sure why I can't get a simple react page to run though

---
+++ policingappmit/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,19 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "policingappmite42503d8": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  },
+  "storage": {
+    "policingstorage": {
+      "BucketName": "string",
+      "Region": "string"
+    }
+  }
+}
